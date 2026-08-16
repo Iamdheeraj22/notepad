b27c0ba5103cce031cd0995af4d967cb15e8c297
imdheeraj <dheerajprajapati334@gmail.com>
2026-08-16 22:23:51 +0530

    feat: implement File Info dialog to display metadata for saved files and add corresponding menu option

--- ui/menu.py
+++ ui/menu.py
@@ -1,4 +1,6 @@
 from tkinter import *
+import os
+import time
 from services.file_service import FileService
 
 class Menubar():
@@ -16,6 +18,7 @@ class Menubar():
         self.configFileMenu()
         self.configEditMenu()
         self.configViewMenu()
+        self.configInfoMenu()
 
     def configFileMenu(self):
         fileMenu = Menu(self.mainMenu)
@@ -72,6 +75,59 @@ class Menubar():
         opened = self.fService.openFile()
         if opened and self.editor:
             self.editor.insertData()
+
+    def configInfoMenu(self):
+        infoMenu = Menu(self.mainMenu)
+        self.mainMenu.add_cascade(label="Info", menu=infoMenu)
+        infoMenu.add_command(label="File Info", command=self.show_file_info)
+
+    def show_file_info(self):
+        from ui.dialogs.file_info_dialog import FileInfoDialog
+        from tkinter import messagebox
+        
+        file_name = self.fService.getFileName()
+        file_path = self.fService.getFilePath()
+        
+        if not file_path or file_name == "untitled" or not file_name:
+            messagebox.showwarning("Warning", "File information not showing until file is saved in the system.")
+            return
+
+        file_info = {
+            "File Name": file_name,
+            "File Path": "",
+            "File Size": "",
+            "Created Date": "",
+            "Modified Date": "",
+            "Read Only Status": "",
+            "Encoding": ""
+        }
+
+        full_path = os.path.join(file_path, file_name)
+        if os.path.exists(full_path):
+            stat = os.stat(full_path)
+            
+            # File Path
+            file_info["File Path"] = full_path
+            
+            # File Size
+            size_kb = stat.st_size / 1024
+            file_info["File Size"] = f"{size_kb:.2f} KB"
+            
+            # Dates
+            created_time = time.localtime(stat.st_ctime)
+            modified_time = time.localtime(stat.st_mtime)
+            file_info["Created Date"] = time.strftime('%Y-%m-%d %H:%M:%S', created_time)
+            file_info["Modified Date"] = time.strftime('%Y-%m-%d %H:%M:%S', modified_time)
+            
+            # Read Only Status
+            is_read_only = not os.access(full_path, os.W_OK)
+            file_info["Read Only Status"] = "Yes" if is_read_only else "No"
+            
+            # Encoding
+            file_info["Encoding"] = "UTF-8" # editor.py defaults to utf-8
+
+        dialog = FileInfoDialog(self.root, file_info)
+        self.root.wait_window(dialog)
 
     def showMenu(self):
         self.configMenu()
